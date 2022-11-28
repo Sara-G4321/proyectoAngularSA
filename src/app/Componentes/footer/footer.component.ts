d0cfdc42509a3f717822aeb699abf28df32c6a47
Sara Gabriela Arteaga <sgarteaga@siesa.com>
2022-11-27 19:10:35 -0500

    Proyecto angular componentes

---
+++ src/app/Componentes/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+public nombre:string='Sara Arteaga'
+public derechos:string='Cali, Colombia - © 2022 Copyright:'
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
+import { AsideComponent } from './Componentes/aside/aside.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { QuienesSomosComponent } from './Componentes/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './Componentes/servicios/servicios.component';
+import { TestimoniosComponent } from './Componentes/testimonios/testimonios.component';
+import { ContactoComponent } from './Componentes/contacto/contacto.component';
+
+//Rutas
+const routes: Routes = [
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'QuienesSomos', component: QuienesSomosComponent},
+  {path: 'Servicios', component: ServiciosComponent},
+  {path: 'Testimonios', component: TestimoniosComponent},
+  {path: 'Contactos', component: ContactoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AsideComponent,
+    InicioComponent,
+    QuienesSomosComponent,
+    ServiciosComponent,
+    TestimoniosComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
